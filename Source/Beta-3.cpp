23f2c0de7b6bb96daa384b7ecdec773458516af3
Marek Dědič <developer@dedic.eu>
2013-02-02 23:27:42 +0100

    Beta-3 Fix

--- Source/Beta-3.cpp
+++ Source/Beta-3.cpp
@@ -50,8 +50,6 @@ void GLTexture::load(string filename)
         glTexImage2D(GL_TEXTURE_2D, 0, mode, image->w, image->h, 0, mode, GL_UNSIGNED_BYTE, image->pixels);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         w = image->w;
         h = image->h;
         SDL_FreeSurface(image);
